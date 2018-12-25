a32372370f61cb06b2a035f7238c1cb67463420c
Bosak Dmytro <bosak.dmitrii@gmail.com>
2018-12-25 23:37:37 +0200

    exception handler

---
+++ ordersmanagement/ordersmanagement/Middlewares/ExceptionHandler/Models/ExceptionDescriptionModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OrdersManagement.RespionseExceptions;
+
+namespace OrdersManagement.Middlewares.ExceptionHandler.Models
+{
+    public class ExceptionDescriptionModel
+    {
+        [JsonIgnore]
+        public int StatusCode { set; get; }
+        public Dictionary<string, string> ErrorDataDictionary { set; get; }
+
+        private ExceptionDescriptionModel() { }
+
+        public static ExceptionDescriptionModel FromCustomException(BaseResponseException exception)
+        {
+            return new ExceptionDescriptionModel
+            {
+                StatusCode = exception.StatusCode,
+                ErrorDataDictionary = exception.ErrorData
+            };
+        }
+
+        public static ExceptionDescriptionModel FromCommonException(Exception ex)
+        {
+            var a = new ExceptionDescriptionModel
+            {
+                StatusCode = 500,
+                ErrorDataDictionary = new Dictionary<string, string> { { ex.Message, ex.StackTrace } }
+            };
+            return a;
+        }
+    }
+}
---
+++ ordersmanagement/ordersmanagement/RespionseExceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersManagement.RespionseExceptions
+{
+    public class NotFoundException : BaseResponseException
+    {
+        public override int StatusCode { get; } = 404;
+
+        public NotFoundException(Dictionary<string, string> errors) : base(errors)
+        {
+        }
+    }
+}
