393216e8da57ba26b798b5f50ea8478aa48eb490
Natalie Vegerina <natalie.vegerina@gmail.com>
2018-12-31 17:23:44 +0200

    Added mongodb; Added json-patch

---
+++ programs-service/src/models/mongoose/baseSchemaOptions.js
@@ -0,0 +1,12 @@
+baseSchema = {
+    toJSON: {
+        transform: function(doc, ret, options) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
+    }
+};
+
+module.exports = baseSchema;
